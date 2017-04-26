8dfebbacb97397ed1625865fabdd6f48cb98e2da
Sean <shaun.n10b@gmail.com>
2017-04-26 12:42:56 +1000

    raining

--- bg.js
+++ bg.js
@@ -1,2 +1,11 @@
 
-$(body).css("background-image", "url(/food01.jpg)")
+$(document).ready(function() {
+
+  //$('body').css("background-image", "url('food01.jpg')");
+  $('body').css("background-color", "black");
+  
+  $('li').css('border-radius', '25px');
+  $('li.dropdown').css('border-radius', '25px');
+  $('.dropdown.li').css('border-radius', '25px');
+
+})
