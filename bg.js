028ab6d921e120933ffffff86949b99b9f73ec45
Sean <shaun.n10b@gmail.com>
2017-05-15 15:36:55 +1000

    version 15 May

--- bg.js
+++ bg.js
@@ -2,8 +2,8 @@
 $(document).ready(function() {
 
   //$('body').css("background-image", "url('food01.jpg')");
-  $('body').css("background-color", "black");
-  
+  //$('body').css("background-color", "black");
+
   $('li').css('border-radius', '25px');
   $('li.dropdown').css('border-radius', '25px');
   $('.dropdown.li').css('border-radius', '25px');
