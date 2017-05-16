4536b8904ffb9b9a62563b46e6bf44b85de48718
Sean <shaun.n10b@gmail.com>
2017-05-16 22:24:20 +1000

    16 May 2017

--- js/bg.js
+++ js/bg.js
@@ -1,11 +1,11 @@
-
+/*
 $(document).ready(function() {
 
-  //$('body').css("background-image", "url('food01.jpg')");
-  //$('body').css("background-color", "black");
+  $('body').css("background-image", "url('images/food01.jpg')");
 
   $('li').css('border-radius', '25px');
   $('li.dropdown').css('border-radius', '25px');
   $('.dropdown.li').css('border-radius', '25px');
 
 })
+*/
